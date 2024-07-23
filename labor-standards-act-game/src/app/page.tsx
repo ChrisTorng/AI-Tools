d84fd2d52cbd7dd7a6947ef5ad1476197cdb0e3c
ChrisTorng <ChrisTorng@hotmail.com>
2024-07-23 23:04:49 +0800

    修正標題。

--- labor-standards-act-game/src/app/page.tsx
+++ labor-standards-act-game/src/app/page.tsx
@@ -2,9 +2,6 @@ import MazeQuizGame from '@/components/labor-law-maze-quiz';
 
 export default function Home() {
   return (
-    <div>
-      <h1>勞動基準法迷宮問答遊戲</h1>
-      <MazeQuizGame />
-    </div>
+    <MazeQuizGame />
   );
 }
