e91be848a10153b6801740f1e7d018f6664c2298
ChrisTorng <ChrisTorng@hotmail.com>
2024-07-24 00:02:14 +0800

    加入 basePath 再測試路徑問題。

--- labor-standards-act-game/next.config.js
+++ labor-standards-act-game/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/AI-Tools/labor-standards-act-game/out',
     assetPrefix: '/AI-Tools/labor-standards-act-game/out',
     output: 'export',
     images: { unoptimized: true },
